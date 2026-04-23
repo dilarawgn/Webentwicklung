a5523f0d37952342753eb1f99ff67f44c295b461
dilara <dilarawgn@gmail.com>
2026-04-23 17:13:08 +0200

    Losfeld Funktion

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ function sayHello() {
   console.log('Hallo');
   addButton.textContent = "Hinzugügen";
   console.log(addButton.id);
-    const input = document.getElementById('wordInput');
+    const input = document.getElementById('wordInput'); // Zugriff auf Inputfeld
     console.log(input.value);
-    const Textfeld = document.getElementById('meinText');
-    Textfeld.textContent += input.value;
+
+    aktuelleLose.push( input.value)
+    displayLose();
 }
+
+let aktuelleLose = [ "Staubsaugen", "Wäsche waschen",]
+  console.log(aktuelleLose);
+  displayLose();
+  storeData();
+
+  function displayLose(){
+    const Losfeld = document.getElementById('meinText');
+    Losfeld.innerHTML = "";
+    for (let los of aktuelleLose){
+      Losfeld.innerHTML += "<li>"+los+"</li>"
+    }
+  }
+
+let Archiv = [ {Listenname: "To Do's", Liste:[ "Staubsaugen", "Wäsche waschen",]},
+{Listenname: "Uni Aufgaben", Liste:[ "AV Video hochladen", "Webentwicklung abgeben",]},
+{Listenname: "Eisauswahl", Liste: [ "Schokolade", "Vanillie", "Erdbeere", "Mango"]},
+{Listenname: "Aktivitäten", Liste: [ "Wandern gehen", "Schwimmbad", "Freizeitpark", "Keramik bemalen"]}
+]
